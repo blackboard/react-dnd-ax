6808865f057098d1c671cc8788b26aedbb964307
ethan.artecher <ethanleenz@gmail.com>
2017-07-20 13:30:01 +0800

    Add test dependencies

---
+++ src/react-dnd-ax/__test__/drag-n-drop-container.test.js
@@ -0,0 +1,5 @@
+describe('Dnd container', () => {
+  it('should render correct template', () => {
+
+  })
+})
---
+++ src/react-dnd-ax/__test__/drag-n-drop-item.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme'
+
+import DragNDropItem from '../drag-n-drop-item'
+
+describe('Dnd items', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+  })
+
+  it('should render correct template', () => {
+
+  })
+})
