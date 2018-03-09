2c9c56ac1a62e469d79fb17d0d3fa23ad27e4f62
Greg Lara <Greg.Lara@blackboard.com>
2018-03-09 09:08:43 -0500

    fix local dev server to reference current code

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import BasicExample from '../src/examples/basic-example/BasicExample'
-import ComplexExample from '../src/examples/complex-example/ComplexExmaple'
+import ComplexExample from '../src/examples/complex-example/ComplexExample'
 storiesOf('Button', module)
   .add('Basic Example', () => (
     <BasicExample />
